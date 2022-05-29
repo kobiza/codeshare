769b5c400db932dcc148b9ec063cdaa40e6d3bd4
kobiz <kobiz@wix.com>
2022-05-29 14:01:17 +0300

    myLodash diff

--- src/kobiz/lodash/myLodash.js
+++ src/kobiz/lodash/myLodash.js
@@ -9,6 +9,20 @@ const chunk = (array, size = 1) => {
     }, [])
 }
 
+const arrToTrueMap = (arr) => {
+    return arr.reduce((acc, v) => {
+        acc.set(v, true)
+        return acc
+    }, new Map())
+}
+
+const difference = (arr1, arr2) => {
+    const arr2TrueMap = arrToTrueMap(arr2)
+
+    return arr1.filter(v => !arr2TrueMap.get(v))
+}
+
 module.exports = {
-    chunk
+    chunk,
+    difference
 }
--- src/kobiz/lodash/myLodash.spec.js
+++ src/kobiz/lodash/myLodash.spec.js
@@ -1,4 +1,4 @@
-const {chunk} = require('./myLodash')
+const {chunk, difference} = require('./myLodash')
 
 describe('my lodash', () => {
     describe('chunk', () => {
@@ -15,4 +15,25 @@ describe('my lodash', () => {
         });
     });
 
+    describe('difference', () => {
+        test('should work', () => {
+            const r = difference([1, 3, 6, 2, 1, 6], [1, 4, 3, 1])
+
+            expect(r).toEqual([6,2,6])
+        });
+
+        test('should work with different types', () => {
+            const r = difference([1, '3', 6, 2, 1], [1, 4, 3, 1])
+
+            expect(r).toEqual(['3', 6, 2])
+        });
+
+        test('should work with objects by ref', () => {
+            var a = {a: 1}
+            var b = {b: 1}
+            const r = difference([a, b], [b, {a: 1}])
+
+            expect(r).toEqual([a])
+        });
+    });
 })
